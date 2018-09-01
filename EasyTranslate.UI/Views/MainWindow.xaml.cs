b1dd4a4cf305e3eb5368af603a3ca0e1e9cd1a88
TheMulti0 <raphael@codeprecise.com>
2018-09-01 16:50:41 +0300

    Init

---
+++ EasyTranslate.UI/Views/MainWindow.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace EasyTranslate.UI.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+}
